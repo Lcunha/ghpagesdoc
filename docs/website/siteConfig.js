39c28f0f624264e55644ac28582db7c54ea40027
Lucas Carvalho <lucascarvalho.esw@gmail.com>
2018-09-01 19:48:36 -0300

    Update siteConfig.js

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://lcunha.github.io/ghpagesdoc/', // Your website URL
+  url: 'https://lcunha.github.io/', // Your website URL
   baseUrl: '/ghpagesdoc/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
