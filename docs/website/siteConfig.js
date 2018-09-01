51832b60d27fafad9fb123d1cabc6c0a9b2420cd
Lucas Carvalho <lucascarvalho.esw@gmail.com>
2018-09-01 16:28:02 -0300

    Update siteConfig.js

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'ghpagesdoc',
-  organizationName: 'Lcunha',
+  organizationName: 'lcunha',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
